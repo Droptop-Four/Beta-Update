d31d8dc78d3ca213325789a6703141ccb1d58b3f
Carib <N/A>
2024-01-07 09:50:01 -0700

    4.340.14

--- Droptop/@Resources/Scripts/AppBuilder/CreateApp.ps1
+++ Droptop/@Resources/Scripts/AppBuilder/CreateApp.ps1
@@ -1,54 +1,58 @@
-$appname=$args[0]
-$appauthor=$args[1]
-$newappname=$args[2]
-$skinspath=$args[3]
-$gitinit=$args[4]
-$programpath=$args[5]
+$ErrorActionPreference= 'silentlycontinue'
 
 $exclude = @('*.rmskin','*.md','Variables.inc','*.org')
 
-$ErrorActionPreference= 'silentlycontinue'
+New-Item -ItemType "directory" -Path "${skinsPath}Droptop Folders\Other files\@Rmskins\Droptop Apps"
+Remove-Item -Path "${skinsPath}Droptop Folders\Other files\@Rmskins\Droptop Apps\$newappname - $appauthor (Droptop App).rmskin" -Recurse
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\*.rmskin" -Recurse
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\*.zip" -Recurse
 
-cd "$skinspath"
-New-Item -ItemType "directory" -Path ".\Droptop Folders\Other files\@Rmskins\Droptop Apps"
-Remove-Item -Path ".\Droptop Folders\Other files\@Rmskins\Droptop Apps\$newappname - $appauthor (Droptop App).rmskin" -Recurse
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\@Rmskins\*.rmskin" -Recurse
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\@Rmskins\*.zip" -Recurse
+Copy-Item -Path "${skinsPath}Droptop Community Apps\Apps\$appname" -Destination "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\" -Recurse -Exclude $exclude
 
-Copy-Item -Path ".\Droptop Community Apps\Apps\$appname" -Destination ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\" -Recurse -Exclude $exclude
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\.git" -Recurse -Force
+Copy-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\Plugins\32bit\*" -Destination "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\32bit\" -Recurse
+Copy-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\Plugins\64bit\*" -Destination "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\64bit\" -Recurse
+Copy-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\Images\RMSKIN.bmp" -Destination "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\" -Recurse
 
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\.git" -Recurse -Force
-Copy-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\Plugins\32bit\*" -Destination ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\32bit\" -Recurse
-Copy-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\Plugins\64bit\*" -Destination ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\64bit\" -Recurse
-Copy-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\$appname\Images\RMSKIN.bmp" -Destination ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\" -Recurse
+# ${skinsPath}Droptop\@Resources\Scripts\AppBuilder\start-process -FilePath "${skinsPath}Droptop Community Apps\Apps\$appname\Scripts\AutoCompile.ps1"
 
-&".\Droptop Community Apps\Apps\$appname\Scripts\AutoCompile.ps1"
+# & "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\MakeRmSkin.ps1"
 
-cd "$skinspath"
-cd .\Droptop\@Resources\Scripts\AppBuilder
-.\MakeRmSkin.ps1 -Skin AppTemplate
-cd "$skinspath"
+$skins = (Get-ChildItem -Directory).Name.ToLower()
+#if ($Skin.ToLower() -notin $skins) {
+#    Write-Output "$Skin is not a valid template!"
+#    return
+#}
 
-Move-Item -Path '.\Droptop\@Resources\Scripts\AppBuilder\@Rmskins\AppTemplate.rmskin' -Destination ".\Droptop Folders\Other files\@Rmskins\Droptop Apps\$newappname - $appauthor (Droptop App).rmskin"
-Copy-Item -Path ".\Droptop Folders\Other files\@Rmskins\Droptop Apps\$newappname - $appauthor (Droptop App).rmskin" -Destination ".\Redistributables\Droptop-Community-Apps\Apps" -Recurse
+[System.IO.Directory]::CreateDirectory("${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins") | Out-Null
+Write-Host ${skinsPath}Droptop\@Resources\Scripts\AppBuilder
 
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\*" -Recurse
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\32bit\*" -Recurse
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\64bit\*" -Recurse
-Remove-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\@Rmskins\*.zip" -Recurse
+Get-ChildItem -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\" -rec -file *.* | Where-Object { $_.LastWriteTime -lt (Get-Date).AddYears(-20) } | ForEach-Object { try { $_.LastWriteTime = '01/01/2020 00:00:00' } catch {} }
+
+Compress-Archive -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\*" -DestinationPath "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\AppTemplate.zip" -CompressionLevel Optimal -Force -ErrorAction Stop
+Rename-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\AppTemplate.zip" -NewName "AppTemplate.rmskin" -Force -ErrorAction Stop
+&"${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AddRmFooter" "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\AppTemplate.rmskin"
+
+
+Move-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\AppTemplate.rmskin" -Destination "${skinsPath}Droptop Folders\Other files\@Rmskins\Droptop Apps\$newappname - $appauthor (Droptop App).rmskin"
+# Copy-Item -Path "${skinsPath}Droptop Folders\Other files\@Rmskins\Droptop Apps\$newappname - $appauthor (Droptop App).rmskin" -Destination "${skinsPath}Redistributables\Droptop-Community-Apps\Apps" -Recurse
+
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Skins\Droptop Community Apps\Apps\*" -Recurse
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\32bit\*" -Recurse
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\AppTemplate\Plugins\64bit\*" -Recurse
+Remove-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\@Rmskins\*.zip" -Recurse
 
 if ($gitinit -eq 1)
 {
-    git init ".\Droptop Community Apps\Apps\$appname"
-	Rename-Item -Path ".\Droptop Community Apps\Apps\$appname\README.md" -NewName "README (Backup).md" -Force -ErrorAction Stop
-	Copy-Item -Path ".\Droptop\@Resources\Scripts\AppBuilder\README.md" -Destination ".\Droptop Community Apps\Apps\$appname" -Recurse
-	(Get-Content ".\Droptop Community Apps\Apps\$appname\README.md") -replace "Your App Name - Author Name","$newappname - $appauthor" | out-file ".\Droptop Community Apps\Apps\$appname\README.md"
-	Set-Content -NoNewline -Encoding OEM ".\Droptop Community Apps\Apps\$appname\README.md" -Value (Get-Content -Raw -Encoding 'utf8' ".\Droptop Community Apps\Apps\$appname\README.md")
+    git init "${skinsPath}Droptop Community Apps\Apps\$appname"
+	Rename-Item -Path "${skinsPath}Droptop Community Apps\Apps\$appname\README.md" -NewName "README (Backup).md" -Force -ErrorAction Stop
+	Copy-Item -Path "${skinsPath}Droptop\@Resources\Scripts\AppBuilder\README.md" -Destination "${skinsPath}Droptop Community Apps\Apps\$appname" -Recurse
+	(Get-Content "${skinsPath}Droptop Community Apps\Apps\$appname\README.md") -replace "Your App Name - Author Name","$newappname - $appauthor" | out-file "${skinsPath}Droptop Community Apps\Apps\$appname\README.md"
+	Set-Content -NoNewline -Encoding OEM "${skinsPath}Droptop Community Apps\Apps\$appname\README.md" -Value (Get-Content -Raw -Encoding 'utf8' "${skinsPath}Droptop Community Apps\Apps\$appname\README.md")
 }
 
 Start-Process -FilePath "$programpath" -ArgumentList "!CommandMeasure", "LoadingTimer", '"Execute 2"', '"Droptop\Other\WindowMenu"'
 
 Start-Sleep -Seconds 3
 
-Invoke-Item '.\Droptop Folders\Other files\@Rmskins\Droptop Apps'
-
+Invoke-Item "${skinsPath}Droptop Folders\Other files\@Rmskins\Droptop Apps"
--- Droptop/@Resources/Scripts/Powershell/GetFontName.ps1
+++ Droptop/@Resources/Scripts/Powershell/GetFontName.ps1
@@ -1,11 +1,4 @@
-﻿$filePath=$args[0]
-$fileName=$args[1]
-$extension=$args[2]
-$skinsPath=$args[3]
-$programPath=$args[4]
-$themeNum=$args[5]
-
-Add-Type -AssemblyName System.Drawing
+﻿﻿Add-Type -AssemblyName System.Drawing
 $path = "$filePath$fileName$extension"
 
 $fontFiles = Get-ChildItem $path
@@ -20,23 +13,17 @@ $fontFiles | ForEach-Object {
 
 Rename-Item -Path "$path" -NewName ${fontName}"$extension" -Force
 
-cd "$skinsPath"
+Remove-Item -Path "${skinsPath}Droptop Folders\Other files\Themes\*.ttf" -Recurse
+Remove-Item -Path "${skinsPath}Droptop Folders\Other files\Themes\*.otf" -Recurse
 
-Remove-Item -Path ".\Droptop Folders\Other files\Themes\*.ttf" -Recurse
-Remove-Item -Path ".\Droptop Folders\Other files\Themes\*.otf" -Recurse
+Copy-Item "$filePath$fontName$extension" -Destination "${skinsPath}Droptop\@Resources\Fonts" -Recurse
+Copy-Item "$filePath$fontName$extension" -Destination "${skinsPath}Droptop Community Apps\@Resources\Fonts" -Recurse
+Copy-Item "$filePath$fontName$extension" -Destination "${skinsPath}Droptop Folders\Other files\Themes" -Recurse
 
-Copy-Item "$filePath$fontName$extension" -Destination ".\Droptop\@Resources\Fonts" -Recurse
-Copy-Item "$filePath$fontName$extension" -Destination ".\Droptop Community Apps\@Resources\Fonts" -Recurse
-Copy-Item "$filePath$fontName$extension" -Destination ".\Droptop Folders\Other files\Themes" -Recurse
+Remove-Item -Path "${skinsPath}Droptop Folders\Other files\Themes\*.*tf" -Recurse
 
-cd "${skinsPath}Droptop Folders\Other files\Themes"
+Copy-Item "${skinsPath}Droptop\@Resources\Fonts\${fontName}.*" "${skinsPath}Droptop Folders\Other files\Themes"
 
-Remove-Item -Path ".\*.*tf" -Recurse
-
-cd "${skinsPath}Droptop\@Resources\Fonts"
-
-Copy-Item "${fontName}.*" "${skinsPath}Droptop Folders\Other files\Themes"
-
-Start-Process -FilePath "$programPath" -ArgumentList "!WriteKeyValue","Variables","FontName0","$fontName","${skinspath}Droptop\@Resources\Themes\${themeNum}Settings.inc"
-Start-Process -FilePath "$programPath" -ArgumentList "!WriteKeyValue","Variables","FontName1","$fontName","${skinspath}Droptop\@Resources\Themes\${themeNum}Settings.inc"
-Start-Process -FilePath "$programPath" -ArgumentList "!RefreshGroup","DroptopSuite"
+Start-Process -FilePath "`"$programPath`"" -ArgumentList "!WriteKeyValue","Variables","FontName0","`"$fontName`"","`"${skinspath}Droptop\@Resources\Themes\${themeNum}Settings.inc`""
+Start-Process -FilePath "`"$programPath`"" -ArgumentList "!WriteKeyValue","Variables","FontName1","`"$fontName`"","`"${skinspath}Droptop\@Resources\Themes\${themeNum}Settings.inc`""
+Start-Process -FilePath "`"$programPath`"" -ArgumentList "!RefreshGroup","DroptopSuite"
