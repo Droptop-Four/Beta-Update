54dd94ee2bf44abee692eca3ae165708f1b95fc6
Carib <N/A>
2024-04-01 16:14:30 -0600

    4.350.21

--- Droptop/@Resources/Scripts/RestartRainmeter.bat
+++ Droptop/@Resources/Scripts/RestartRainmeter.bat
@@ -1,2 +1,2 @@
-@echo "Transfering files from OneDrive to the local Documents folder... please wait..." & @echo off & robocopy "C:\Users\%USERNAME%\OneDrive\Documents\Rainmeter\Skins" "C:\Users\%USERNAME%\Documents\Rainmeter\Skins" /NFL /NDL /NJH /NJS /nc /ns /np /e & timeout 1 & start "" %1 & exit
+@echo off & taskkill /f /im "Rainmeter.exe" & timeout 3 & start "" %1 & exit
 
