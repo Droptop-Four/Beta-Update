e2c37c90a5dd2634dea8a4b4b3dfe4350548ca49
Carib <N/A>
2026-02-11 20:09:17 -0700

    4.410.39

--- Droptop/@Resources/Scripts/Other/CompileDroptop.bat
+++ Droptop/@Resources/Scripts/Other/CompileDroptop.bat
@@ -170,6 +170,7 @@ REM taskkill /fi "windowtitle eq Droptop_Task_Helper*" /f
 %4 !WriteKeyValue Variables CustomHomeButton 1 %5Droptop\@Resources\GlobalVar\UserSettings.inc"
 %4 !WriteKeyValue Variables HomeColorNum 1 %5Droptop\@Resources\GlobalVar\UserSettings.inc"
 %4 !WriteKeyValue Variables AutohideShowOnDesktop 1 %5Droptop\@Resources\GlobalVar\UserSettings.inc"
+%4 !WriteKeyValue Variables FastLoadNum 0 %5Droptop\@Resources\GlobalVar\UserSettings.inc"
 
 %4 !WriteKeyValue Variables FolderColorNum1 "1" %5Droptop\@Resources\GlobalVar\UserSettings.inc"
 %4 !WriteKeyValue Variables FolderColorNum2 "1" %5Droptop\@Resources\GlobalVar\UserSettings.inc"
