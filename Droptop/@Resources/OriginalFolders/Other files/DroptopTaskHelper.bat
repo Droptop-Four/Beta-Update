7900a090a897ed2c997caacdaa2531a3b1dd3bee
KazukiGames82 <saaggosao@gmail.com>
2026-05-05 16:35:20 -0600

    4.420.72

--- Droptop/@Resources/OriginalFolders/Other files/DroptopTaskHelper.bat
+++ Droptop/@Resources/OriginalFolders/Other files/DroptopTaskHelper.bat
@@ -1,4 +1,28 @@
+@REM @echo off
+@REM title Droptop_Task_Helper
+
+@REM set "app=%~1"
+
+@REM :loop
+@REM tasklist /fi "imagename eq %app%" | find /i "%app%" > nul
+
+@REM if %errorlevel% neq 0 (
+@REM     start "" "%app%"
+@REM )
+
+@REM timeout /t 10 > nul
+@REM goto loop
+
 @echo off
+setlocal enabledelayedexpansion
+
+taskkill /f /im timeout.exe /t >nul 2>&1
+
+tasklist /fi "imagename eq cmd.exe" /v | find "Droptop_Task_Helper" > nul && (
+    echo Already running
+    exit
+)
+
 title Droptop_Task_Helper
 
 set "app=%~1"
@@ -6,7 +30,7 @@ set "app=%~1"
 :loop
 tasklist /fi "imagename eq %app%" | find /i "%app%" > nul
 
-if %errorlevel% neq 0 (
+if !errorlevel! neq 0 (
     start "" "%app%"
 )
 
